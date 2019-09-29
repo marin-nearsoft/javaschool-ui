d9df3311bed998353ea6a0697ab6a639f37d13b1
Daniel Fernandez <fdezfdaniel@gmail.com>
2019-09-28 18:39:03 -0600

    shp-01, using of lombok on RabbitMqProiperties class.

--- webservices/src/main/java/com/javaschool/webservices/configuration/RabbitMQProperties.java
+++ webservices/src/main/java/com/javaschool/webservices/configuration/RabbitMQProperties.java
@@ -3,36 +3,15 @@ package com.javaschool.webservices.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Component
 @ConfigurationProperties("rabbitmq")
 public class RabbitMQProperties {
 
-	private String exchange;
-	private String routingKey;
-	private String shippingQueueName;
-
-	public String getExchange() {
-		return exchange;
-	}
-
-	public void setExchange(String exchange) {
-		this.exchange = exchange;
-	}
-
-	public String getRoutingKey() {
-		return routingKey;
-	}
-
-	public void setRoutingKey(String routingKey) {
-		this.routingKey = routingKey;
-	}
-
-	public String getShippingQueueName() {
-		return shippingQueueName;
-	}
-
-	public void setShippingQueueName(String shippingQueueName) {
-		this.shippingQueueName = shippingQueueName;
-	}
-
+	@Getter @Setter private String exchange;
+	@Getter @Setter private String routingKey;
+	@Getter @Setter private String shippingQueueName;
+	
 }
