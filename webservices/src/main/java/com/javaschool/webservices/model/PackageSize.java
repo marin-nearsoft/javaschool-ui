d0b8ee6f43ba0fe2e0b9572df210f3ca1bc11751
Daniel Fernandez <fdezfdaniel@gmail.com>
2019-10-05 12:44:25 -0600

    shp-03, pr feedback changes, adding getter and setter lombok annotation to class level.

--- webservices/src/main/java/com/javaschool/webservices/model/PackageSize.java
+++ webservices/src/main/java/com/javaschool/webservices/model/PackageSize.java
@@ -5,12 +5,13 @@ import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter @Setter
 public class PackageSize implements Serializable {
 
 	private static final long serialVersionUID = 865846279570747807L;
 	
-	@Getter @Setter private int id;
-	@Getter @Setter private String description;
-	@Getter @Setter private double priceFactor;
+	private int id;
+	private String description;
+	private double priceFactor;
 
 }
--- webservices/src/main/java/com/javaschool/webservices/model/PackageTransport.java
+++ webservices/src/main/java/com/javaschool/webservices/model/PackageTransport.java
@@ -5,11 +5,12 @@ import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter @Setter
 public class PackageTransport implements Serializable{
 
 	private static final long serialVersionUID = 8539127243710282153L;
 	
-	@Getter @Setter private int id;
-	@Getter @Setter private String description;
-	@Getter @Setter private double pricePerMile;
+	private int id;
+	private String description;
+	private double pricePerMile;
 }
